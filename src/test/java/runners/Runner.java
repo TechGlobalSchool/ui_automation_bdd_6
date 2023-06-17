776f4a1fce48c06b43d2654f8374cc6f20983e62
buraksimsekgit <simsek.bl@gmail.com>
2023-06-17 13:19:01 -0500

    etsy page

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@Wiki",
+        tags = "@Etsy",
         dryRun = true,
         monochrome = false,
         plugin = {"pretty",
